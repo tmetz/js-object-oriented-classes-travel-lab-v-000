1c54fac8642784216fa99af274d447da22d7644e
Tammy Metz <tammy@tammymetz.com>
2019-05-27 23:32:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+class Driver {
+  constructor (name, dateString) {
+    this.name = name;
+    this.date = new Date(dateString);
+  }
 
+  startDate() {
+    return this.date;
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    return this.date.year - year;
+  }
+  
+}
