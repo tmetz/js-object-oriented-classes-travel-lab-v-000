2514a5dab2bdf406c96037d75f6a4e96bff4ccd8
Tammy Metz <tammy@tammymetz.com>
2019-05-27 23:53:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
 
+class Driver {
+  constructor (name, dateString) {
+    this.name = name;
+    this.startDate = new Date(dateString);
+  }
+
+  startDate() {
+    return this.startDate;
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    return  Number(year) - this.startDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    let verticalBlocks = Number(this.endingLocation.vertical) - Number(this.beginningLocation.vertical);
+    let horizontalBlocks = eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal);
+    return Math.abs(verticalBlocks) + Math.abs(horizontalBlocks);
+  }
+
+  estimatedTime(peakTime) {
+    if (peakTime) {
+      return this.blocksTravelled() / 2;
+    }
+    else {
+      return this.blocksTravelled() / 3;
+    }
+  }
+}
