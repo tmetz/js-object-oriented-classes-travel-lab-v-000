6c7f4bfbb5b10c1782f95e29872e85b877e8d083
Tammy Metz <tammy@tammymetz.com>
2019-05-27 23:52:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ class Route {
 
   estimatedTime(peakTime) {
     if (peakTime) {
-      return this.blocksTravelled() * 2;
+      return this.blocksTravelled() / 2;
     }
     else {
-      return this.blocksTravelled() * 3;
+      return this.blocksTravelled() / 3;
     }
   }
 }
